7bc286dff47cabf3f814061374e5a041fecf7b76
Francoise Mbazoa Okala <okalis05@yahoo.com>
2026-09-16 22:39:33 -0400

    Remove temporary HPG production catalog loader

--- kaleido/build.sh
+++ kaleido/build.sh
@@ -7,8 +7,5 @@ pip install -r requirements.txt
 echo "Applying database migrations..."
 python manage.py migrate --noinput
 
-echo "Loading finalized HPG production catalog..."
-python manage.py load_hpg_production_catalog
-
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
