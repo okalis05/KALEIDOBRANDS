d98c76ecccc5bbf11087f33bb2b88e2d2e3063d1
Francoise Mbazoa Okala <okalis05@yahoo.com>
2026-09-16 20:44:03 -0400

    Temporarily sync GC16 during production build

--- kaleido/build.sh
+++ kaleido/build.sh
@@ -9,3 +9,6 @@ python manage.py migrate --noinput
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "Syncing production HPG test product GC16..."
+python manage.py sync_hpg --product-id GC16
